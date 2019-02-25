c99f711166ab28df2aff52ab910ac08da7d512d8
wansong <betterofsong@gmail.com>
2019-02-25 23:23:16 +0800

    rb: remove_min

--- lib/bst.h
+++ lib/bst.h
@@ -1,5 +1,10 @@
 #include <iostream>
 
+#define IS_2(n) (n && !n ->red() && (!n ->left || !n ->left ->red()) && (!n ->right || !n ->right ->red()))
+#define IS_3(n) (n && !n ->red() && n ->left && n ->left ->red() && (!n ->right || !n ->right ->red()))
+#define IS_4(n) (n && !n ->red() && n ->left && n ->left ->red() && n ->right && n ->right ->red())
+#define IS_RED(n) (n && n ->red())
+
 // TODO: wansong, 封装
 template <typename T>
 struct Node {
@@ -31,6 +36,34 @@ public:
 
   void remove (T val) {
     std::cout << "remove: todo" << std::endl;
+  }
+
+  Node<T>* remove_min () {
+    if (!root) {
+      return NULL;
+    }
+    auto left = root ->left;
+    auto right = root ->right;
+    if (!right) {
+      if (left) {
+        assert(left ->red());
+        node ->left = NULL;
+        return left;
+      } else {
+        auto node = root;
+        root = NULL;
+        return node 
+      }
+    }
+
+    // root及左右都是2节点，变成4节点
+    if (!IS_3(root) && !IS_3(left) && !IS_3(right)) {
+      left ->is_red = true;
+      right ->is_red = true;
+    }
+    auto res_rm = remove_min_imp(root);
+    root = res_rm.first;
+    return res_rm.second;
   }
 
 private:
@@ -79,6 +112,7 @@ private:
     return ret;
   }
 
+private:
   static Node<T> *rotate_left(Node<T> *node) {
     assert(node ->right ->red());
     assert(!node ->left || !node ->left ->red());
@@ -109,5 +143,70 @@ private:
     node ->right ->is_red = false;
     node ->is_red = true;
     return node;
+  }
+
+private:
+  std::pair<Node<T> *, Node<T> *> remove_min_imp(Node<T> *node) {
+    auto left = node ->left;
+    auto right = node ->right;
+    auto left_child = IS_2(node) ? left : left ->left;
+    auto left_sibling = IS_2(node) ? right : left ->right;
+    if (!left_child) {
+      // 叶节点（3或4）
+      auto was_4 = IS_4(node);
+      node ->left = NULL;
+      auto ret = was_4 ? rotate_left(node) : node;
+      return std::make_pair(ret, left);
+    } else if (!IS_2(left_child)) {
+      ; // noop
+    } else if (!IS_2(left_sibling)) {
+      // 从右边借
+      assert(IS_3(left_sibling));
+      auto from_sibling = left_sibling ->left;
+      auto shift_left = IS_2(node) ? node : left;
+      from_sibling ->is_red = shift_left ->is_red;
+      left_sibling ->left = from_sibling ->right;
+      from_sibling ->right = left_sibling;
+      shift_left ->right = from_sibling ->left;
+      from_sibling ->left = shift_left;
+      left_child ->is_red = true;
+      if (IS_2(node)) {
+        node = from_sibling;
+      } else {
+        node ->left = from_sibling;
+      }
+    } else {
+      // 合并4节点
+      left ->is_red = false;
+      left ->left ->is_red = true;
+      left ->right ->is_red = true;
+      if (IS_4(node)) {
+        node = rotate_left(node);
+      }
+    }
+    // 结构改变后更新相关变量
+    left = node ->left;
+    right = node ->right;
+    left_child = IS_2(node) ? left : left ->left;
+
+    auto ret_rm = remove_min_imp(left_child);
+    if (IS_2(node)) {
+      node ->left = ret_rm.first;
+    } else {
+      left ->left = ret_rm.first;
+    }
+
+    auto rm = ret_rm.second;
+    if (!IS_RED(node ->left) && IS_RED(node ->right)) {
+      node = rotate_left(node);
+    }
+    if (IS_RED(node ->left) && node ->left && IS_RED(node ->left ->left)) {
+      node = rotate_right(node);
+    }
+    if (IS_RED(node ->left) && IS_RED(node ->right)) {
+      node = flip_color(node);
+    }
+
+    return std::make_pair(node, rm);
   }
 };
